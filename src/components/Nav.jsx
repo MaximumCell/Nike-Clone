781965da084ffb1c8b78277add69b27d0104b012
MaximumCell <alihassanwinner@gmail.com>
2025-02-05 21:11:57 +0500

    nothing major

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ const Nav = () => {
   return (
     <header className= "padding-x py-8 absolute z-10 w-full">
         <nav className="flex justify-between items-center max-container">
-            <a href="">
+            <a href="#">
                 <img src={headerLogo} alt="Nike logo" width={130} height={29} />
             </a>
             <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
